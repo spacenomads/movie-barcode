74748cbc9251843d08943647fc353dd58aacf6be
Carlos Mañas <carlos@spacenomads.com>
2022-01-23 12:45:01 +0100

    Use an input type "file" to load the user video

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ const body = document.querySelector('body');
 const video = body.querySelector('#video');
 const dimensions = body.querySelector('#dimensions');
 const duration = body.querySelector('#duration');
+const videoUploader = body.querySelector('.js__file-field');
+
+
+
 
 
 function getDimensions(player) {
   return `${player.videoWidth}x${player.videoHeight}`;
 }
+
+
+
 
 
 function getDuration(player) {
@@ -16,9 +23,34 @@ function getDuration(player) {
   return time;
 }
 
-console.log(video);
+
+
+
+
+function playSelectedFile(event) {
+  const field = event.currentTarget;
+  const file = field.files[0];
+  const { type } = file;
+  console.log({type});
+
+
+  const canPlay = !!video.canPlayType(type);
+  if (!canPlay) {
+    console.error(`Can't play type: ${type}`);
+    return;
+  }
+
+  const fileURL = URL.createObjectURL(file);
+  video.src = fileURL;
+}
+
+
+
+
 
 video.addEventListener('loadedmetadata', () => {
   dimensions.innerHTML = getDimensions(video);
   duration.innerHTML = getDuration(video);
 });
+
+videoUploader.addEventListener('change', playSelectedFile);
